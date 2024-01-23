c2f35215108cd7c9a5601943cc4bf9d2ee73ab44
Tri Bayu Priangga <tpriangga6@gmail.com>
2024-01-23 23:52:32 +0700

    Add About section to views list

--- lib/app/utils/utils.dart
+++ lib/app/utils/utils.dart
@@ -3,9 +3,10 @@ import 'package:webportofolio/app/sections/home/home.dart';
 import 'package:webportofolio/app/sections/services/services.dart';
 
 import '../../core/providers/scroll_provider.dart';
+import '../sections/about/about.dart';
 
 class BodyUtils {
   static List<Widget> views(ScrollProvider scrollProvider) {
-    return [const Home(), const Services()];
+    return [const Home(), const Services(), const About()];
   }
 }
