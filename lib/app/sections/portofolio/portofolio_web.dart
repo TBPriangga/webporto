3607ead43549b065a52a760812737540061cd7b9
Tri Bayu Priangga <tpriangga6@gmail.com>
2024-02-02 00:04:00 +0700

    Add tab controller and tab bar to PortofolioWeb

--- lib/app/sections/portofolio/portofolio_web.dart
+++ lib/app/sections/portofolio/portofolio_web.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:webportofolio/core/color/colors.dart';
+import 'package:webportofolio/widgets/navbar_work_button.dart';
 
 class PortofolioWeb extends StatefulWidget {
   const PortofolioWeb({super.key});
@@ -9,7 +10,28 @@ class PortofolioWeb extends StatefulWidget {
   State<PortofolioWeb> createState() => _PortofolioWebState();
 }
 
-class _PortofolioWebState extends State<PortofolioWeb> {
+class _PortofolioWebState extends State<PortofolioWeb>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+  final _tabs = [
+    const Tab(
+      child: NavBarWorkButton(
+        label: 'All',
+      ),
+    ),
+    const Tab(child: NavBarWorkButton(label: 'Mobile App')),
+    const Tab(child: NavBarWorkButton(label: 'Web App')),
+    const Tab(child: NavBarWorkButton(label: 'UI/UX Design')),
+    const Tab(child: NavBarWorkButton(label: 'Back End')),
+  ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -69,7 +91,23 @@ class _PortofolioWebState extends State<PortofolioWeb> {
                       fontWeight: FontWeight.w900),
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 5.h,
+            ),
+            SizedBox(
+              height: 50.0,
+              child: TabBar(
+                tabs: _tabs,
+                controller: _tabController,
+                labelPadding: const EdgeInsets.all(0),
+                indicatorSize: TabBarIndicatorSize.label,
+                labelColor: Colors.white,
+                indicatorColor: primaryColor,
+                indicatorWeight: 4,
+                unselectedLabelColor: Colors.black,
+              ),
+            ),
           ],
         ),
       ],
